79b94d49ecf7f5ef76c73ec4fb2e6de1800d6aee
tdbah1 <tiendan777@gmail.com>
2022-08-14 14:53:03 -0400

    update to push to MAIN not MASTER

--- src/main/java/com/example/demo/controller/PersonController.java
+++ src/main/java/com/example/demo/controller/PersonController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class PersonController {
 	@RequestMapping("/getname")
 	public String getName() {
-		return "Pushing to Github with SSHkey";
+		return "Pushing to Github with SSHkey to MAIN or MASTER";
 	}
 
 }
